be430c88e2225f71f249e3a828120d5e7119b996
dimonalek <dimonalek@gmail.com>
2026-03-30 13:23:51 +0300

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -8,13 +8,17 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        if "vaccine" not in visitor or not visitor["vaccine"]:
-            raise errors_pkg.NotVaccinatedError("Visitor is not vaccinated")
+        if "vaccine" not in visitor or not visitor.get("vaccine"):
+            raise errors_pkg.NotVaccinatedError(
+                "Visitor is not vaccinated"
+            )
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise errors_pkg.OutdatedVaccineError(
-                "Visitor has an outdated vaccine"
+                "Vaccine is outdated"
             )
-        elif "wearing_a_mask" not in visitor or not visitor["wearing_a_mask"]:
+        elif "wearing_a_mask" not in visitor or not visitor.get(
+            "wearing_a_mask"
+        ):
             raise errors_pkg.NotWearingMaskError(
                 "Visitor is not wearing a mask"
             )
--- app/main.py
+++ app/main.py
@@ -3,19 +3,18 @@ import app.errors as errors_pkg
 
 
 def go_to_cafe(friends: list[dict], cafe: cafe_pkg.Cafe) -> str:
-    group_cant_visit = False
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except errors_pkg.VaccineError:
-            group_cant_visit = True
-            message = "All friends should be vaccinated"
+            return "All friends should be vaccinated"
         except errors_pkg.NotWearingMaskError:
-            group_cant_visit = True
-            masks_to_buy = sum(
-                1 for friend in friends if not friend.get("wearing_a_mask")
-            )
-            message = f"Friends should buy {masks_to_buy} masks"
-    if group_cant_visit:
-        return message
+            pass
+
+    masks_to_buy = sum(
+        1 for friend in friends if not friend.get("wearing_a_mask")
+    )
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+
     return f"Friends can go to {cafe.name}"
